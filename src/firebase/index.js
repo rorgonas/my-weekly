fb68335e08529c6bba6006b11896646871ab7f3d
rorgonas <rolland.orgonas@productionstmv.com>
2019-09-25 12:24:52 -0400

    add - env variable

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,16 +1,15 @@
 import firebase from 'firebase';
-import env from './config';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: env.API_KEY,
-  authDomain: env.AUTH_DOMAIN,
-  databaseURL: env.DATABASE_URL,
-  projectId: env.PROJECT_ID,
-  storageBucket: env.STORAGE_BUCKET,
-  messagingSenderId: env.MESSAGING_SENDER_ID,
-  appId: env.APP_ID,
-  measurementId: env.measurementId,
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.measurementId,
 };
 
 // Initialize Firebase
--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
+import dotenv from 'dotenv';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
 import '@/firebase/';
+
+dotenv.config();
 
 Vue.config.productionTip = false;
 
